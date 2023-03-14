9661c03622b443ef1746abf1c455624322342833
Ruben <rubenmontes330@gmail.com>
2023-03-14 20:04:31 +0000

    ejemplo de redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import {createStore} from 'redux'
+
+const store= createStore((state= 0, action)=>{ //Es un reducer????
+  //action= {type: 'tipo de accion', payload :any}
+  switch(action.type){
+    case 'incrementar':{
+      return state+1
+    }
+    case 'decrementar':{
+      return state-1
+    }
+    default: 
+      return state
+  }
+  
+})
+console.log(store.getState());
+store.dispatch({ type: 'lala'})
+console.log(store.getState());
+store.dispatch({ type: 'incrementar'})
+console.log(store.getState());
+store.dispatch({ type: 'decrementar'})
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +34,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
